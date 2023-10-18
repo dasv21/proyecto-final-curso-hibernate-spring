b379624de14853a3267c1f43f2c61da5cb626920
dasv21 <davidsantiago434@gmail.com.com>
2023-10-18 12:13:34 -0400

    inicio de proyecto final del curso de hiberate con spring boot

---
+++ src/main/java/com/santiago/proyectohibernate/ProyectoHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.proyectohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiago/proyectohibernate/ProyectoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.proyectohibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
